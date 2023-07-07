29d3ab2f5c3af3e5d3febe8ccecc174f7ab7200e
Chris <chris@cotex.com.tw>
2023-07-07 15:56:44 +0800

    0707_加入 bootstrap Card+顯示圖片

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ import {
   DropdownToggle, DropdownMenu, DropdownItem
 } from 'reactstrap';
 
-const baseURL = "http://127.0.0.1:8000/v1/products/items/?skip=0&limit=10";
+// const baseURL = "http://localhost:8000/v1/products/items_img/?skip=0&limit=100";
+const baseURL = "http://127.0.0.1:8000/v1/products/clothes_img/?clothes_no=C50021"
+//上線API網址
+// const baseURL = "https://simurgh01-qds5m4plcq-de.a.run.app/v1/products/items/?skip=0&limit=10";
 
 function App() {
   const [post, setPost] = React.useState(null);
@@ -34,29 +37,41 @@ const clothesData=()=>{
         //   </li>
         //   <Button color="danger">danger</Button>
         // </ol>
-        <tr>
-          <th scope="row">{i+1}</th>
-          <td>{post[i].clothes_no}</td>
-          <td>{post[i].clothes_name}</td>
-        </tr>
+
+        // <tr>
+        //   <th scope="row">{i+1}</th>
+        //   <td>{post[i].clothes_no}</td>
+        //   <td>{post[i].clothes_name}</td>
+        // </tr>
+            <div class="col-sm">
+                <div class="card" style={{width: '18rem'}}>
+                  <img class="card-img-top" src={post[i].clothes_img} alt="Card image cap" />
+                  <div class="card-body">
+                    <h5 class="card-title">{post[i].clothes_no}</h5>
+                    <p class="card-text">{post[i].clothes_name}</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+            </div>
+
+        //
+        // <div class="card" style={{width: '18rem'}}>
+        //   <img class="card-img-top" src={post[i].clothes_img} alt="Card image cap" />
+        //   <div class="card-body">
+        //     <h5 class="card-title">{post[i].clothes_no}</h5>
+        //     <p class="card-text"></p>
+        //     <a href="#" class="btn btn-primary">Go somewhere</a>
+        //   </div>
+        // </div>
         )
       }
     return output;
 }
   return (
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">款號</th>
-      <th scope="col">款名</th>
-    </tr>
-  </thead>
-  <tbody>
-    {clothesData()}
-  </tbody>
-</table>
-    
+    <div class="container">
+    <div class="row">
+      {clothesData()}
+    </div></div>
   );
   
 }
