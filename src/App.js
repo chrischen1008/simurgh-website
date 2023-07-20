e919f9ac6f784d38b72d1952f025f3e7c69edd89
Chris <chris@cotex.com.tw>
2023-07-20 15:18:50 +0800

    class -> className

--- src/App.js
+++ src/App.js
@@ -127,59 +127,59 @@ const clothesGallery=()=>{
 const rightButtons=()=>
 {
   return (
-  <div class="toolbox__container toolbox--bounce-in">
-  <div class="toolbox__item">
+  <div className="toolbox__container toolbox--bounce-in">
+  <div className="toolbox__item">
     <div data-for="of5mvbrsvb" data-tip="tooltip" currentitem="false"><a rel="noreferrer"
-        class="toolbox__button false" href="https://www.instagram.com/simurgh.cc/" target="_blank"
+        className="toolbox__button false" href="https://www.instagram.com/simurgh.cc/" target="_blank"
         style={{fontSize: '44.4444px', width:' 0.9em', height: '0.9em' ,borderRadius: '100%'}}><i
-          class="toolbox__icon icon icon-editor-icon_quick_ig_solid_round"></i>
-          <img src="https://storage.googleapis.com/asia.artifacts.abstract-arbor-392000.appspot.com/static/instagram.png" alt="" class="img-responsive img-rounded"/></a></div>
+          className="toolbox__icon icon icon-editor-icon_quick_ig_solid_round"></i>
+          <img src="https://storage.googleapis.com/asia.artifacts.abstract-arbor-392000.appspot.com/static/instagram.png" alt="" className="img-responsive img-rounded"/></a></div>
           
     {/* <div
-      class="__react_component_tooltip place-left type-dark tooltip--custom tooltip--custom--clickable toolbox__tooltip"
+      className="__react_component_tooltip place-left type-dark tooltip--custom tooltip--custom--clickable toolbox__tooltip"
       id="of5mvbrsvb" data-id="tooltip" style={{left: '1058px', top: '642px'}}><span>Instagram</span></div> */}
   </div>
-  <div class="toolbox__item">
+  <div className="toolbox__item">
     <div data-for="ts3cve9uyv" data-tip="tooltip" currentitem="false"><a rel="noreferrer"
-        class="toolbox__button false" href="https://m.me/SimurghTaiwan" target="_blank"
+        className="toolbox__button false" href="https://m.me/SimurghTaiwan" target="_blank"
         style={{fontSize: '44.4444px', width:' 0.9em', height: '0.9em' ,borderRadius: '100%'}}><i
-          class="toolbox__icon icon icon-editor-icon_messenger"></i><img src="https://storage.googleapis.com/asia.artifacts.abstract-arbor-392000.appspot.com/static/messenger01.png" alt="" class="img-responsive img-rounded"
+          className="toolbox__icon icon icon-editor-icon_messenger"></i><img src="https://storage.googleapis.com/asia.artifacts.abstract-arbor-392000.appspot.com/static/messenger01.png" alt="" className="img-responsive img-rounded"
           /></a></div>
           
     {/* <div
-      class="__react_component_tooltip place-left type-dark tooltip--custom tooltip--custom--clickable toolbox__tooltip"
+      className="__react_component_tooltip place-left type-dark tooltip--custom tooltip--custom--clickable toolbox__tooltip"
       id="ts3cve9uyv" data-id="tooltip" style={{left: '1058px', top: '692px'}}><span>FB Messenger</span></div> */}
   </div>
-  <div class="toolbox__item">
+  <div className="toolbox__item">
     <div data-for="vib121ci11" data-tip="tooltip" currentitem="false"><a rel="noreferrer"
-        class="toolbox__button false" href="https://line.naver.jp/ti/p/@simurgh" target="_blank"
+        className="toolbox__button false" href="https://line.naver.jp/ti/p/@simurgh" target="_blank"
         style={{fontSize: '44.4444px', width:'0.9em' ,height:'0.9em', borderRadius: '100%'}}><i
-          class="toolbox__icon icon icon-editor-icon_line_round"></i>
-          <img src="https://storage.googleapis.com/asia.artifacts.abstract-arbor-392000.appspot.com/static/line.png" alt="" class="img-responsive img-rounded"/></a></div>
+          className="toolbox__icon icon icon-editor-icon_line_round"></i>
+          <img src="https://storage.googleapis.com/asia.artifacts.abstract-arbor-392000.appspot.com/static/line.png" alt="" className="img-responsive img-rounded"/></a></div>
           
     {/* <div
-      class="__react_component_tooltip place-left type-dark tooltip--custom tooltip--custom--clickable toolbox__tooltip"
+      className="__react_component_tooltip place-left type-dark tooltip--custom tooltip--custom--clickable toolbox__tooltip"
       id="vib121ci11" data-id="tooltip" style={{left: '1114px', top: '742px'}}><span>LINE</span></div> */}
   </div>
-  <div class="toolbox__item">
+  <div className="toolbox__item">
     <div data-for="6wr0jzctpi" data-tip="tooltip" currentitem="false"><a rel="noreferrer"
-        class="toolbox__button false"
+        className="toolbox__button false"
         href="https://www.youtube.com/channel/UCnSgp99LeQl1C1CFiQD01eA" target="_blank" style={{fontSize: '40px', width:'0.9em' ,height:'0.9em', borderRadius: '100%'}}><i
-        class="toolbox__icon icon icon-editor-icon_line_round"></i>
+        className="toolbox__icon icon icon-editor-icon_line_round"></i>
         <img
-          loading="lazy" class="img-responsive img-rounded"
+          loading="lazy" className="img-responsive img-rounded"
           src="https://storage.googleapis.com/asia.artifacts.abstract-arbor-392000.appspot.com/static/youtube.png"
           alt="Youtube"/>
         </a>
         </div>
         
     {/* <div
-      class="__react_component_tooltip place-left type-dark tooltip--custom tooltip--custom--clickable toolbox__tooltip"
+      className="__react_component_tooltip place-left type-dark tooltip--custom tooltip--custom--clickable toolbox__tooltip"
       id="6wr0jzctpi" data-id="tooltip" style={{left: '1095px', top: '792px'}}><span>Youtube</span></div> */}
   </div>
-  <div class="toolbox__item">
-    <div><a rel="noreferrer" class="toolbox__button toolbox__button--shadow" target="_blank"
-        style={{fontSize: '40px'}}><i class="toolbox__icon icon icon-top"></i></a></div>
+  <div className="toolbox__item">
+    <div><a rel="noreferrer" className="toolbox__button toolbox__button--shadow" target="_blank"
+        style={{fontSize: '40px'}}><i className="toolbox__icon icon icon-top"></i></a></div>
   </div>
 </div>);
 }
